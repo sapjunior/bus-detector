b4ef5e73a4f374eacc987a50af3579383c329b4f
sapjunior <thananop.k@gmail.com>
2015-08-31 23:35:15 +0700

    Add model file explanation

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 using namespace cv;
 int main()
 {
+    //Use ImageNet Style Model Files
     bus_detector_cnn n("/home/thananop/Works/Pre-Doctoral/matconvnet_converter/imagenet-vgg-verydeep-16_cudnn.mat");
     n.findBus("1.jpg");
     return 0;
